529b1e7f6845a5a250754416f0efb924cc4c79ef
kanishkasd <60418548+kanishkasd@users.noreply.github.com>
2023-08-03 13:44:39 +0530

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<?php
+include_once 'header.php';
+?>
+            <div class="content">
+                <h1>Ceylon Electricity Bill Calculation System</h1>
+                <p>lets create an account and track your electricity consumption  daily/monthly, <br>genarate accurate bill, <br>received warnings for high energy usage and reminders </p>
+
+                <div>
+                    <button type="button"><span></span> LogIn</button>
+                    <button type="button"><span></span> Create an account</button>
+                </div>
+            </div>
+        </div>
+<?php
+include_once 'footer.php';
+?>
+   
